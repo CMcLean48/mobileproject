510bae854bc55655609c15bae469906f4ff37b34
crystal mclean <cmclean48@my.bcit.ca>
2020-03-11 16:05:51 -0700

    no idea

--- stocks/App.js
+++ stocks/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home  from './screens/Home'
+import Home  from './screens/Home';
+import Register from './screens/Register';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Stocks" component={Home} />
+        <Stack.Screen name="Register" component = {Register}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- stocks/screens/Register.js
+++ stocks/screens/Register.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 import GenerateForm from 'react-native-form-builder';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 
 const styles = {
 	wrapper: {
@@ -12,3 +12,47 @@ const styles = {
 		paddingTop: 20
 	}
 };
+// These Fields will create a login form with three fields
+const fields = [
+	{
+		type: 'text',
+		name: 'user_name',
+		required: true,
+		icon: 'ios-person',
+		label: 'Username'
+	},
+	{
+		type: 'password',
+		name: 'password',
+		icon: 'ios-lock',
+		required: true,
+		label: 'Password'
+	}
+];
+export default class FormGenerator extends Component {
+	login() {
+		const formValues = this.formGenerator.getValues();
+		console.log('FORM VALUES', formValues);
+	}
+	render() {
+		return (
+			<View style={styles.wrapper}>
+				<View>
+					<GenerateForm
+						ref={c => {
+							this.formGenerator = c;
+						}}
+						fields={fields}
+					/>
+				</View>
+				<View style={styles.submitButton}>
+					<Button block onPress={() => this.login()}>
+						<Text>Login</Text>
+					</Button>
+				</View>
+			</View>
+		);
+	}
+}
+
+AppRegistry.registerComponent('FormGenerator', () => FormGenerator);
