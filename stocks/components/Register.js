333a64c2e9c189cd238416bbec2e63225d1b2caf
crystal mclean <cmclean48@my.bcit.ca>
2020-03-11 14:39:54 -0700

    form package and register

---
+++ stocks/components/Register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import GenerateForm from 'react-native-form-builder';
+
+const styles = {
+	wrapper: {
+		flex: 1,
+		marginTop: 150
+	},
+	submitButton: {
+		paddingHorizontal: 10,
+		paddingTop: 20
+	}
+};
