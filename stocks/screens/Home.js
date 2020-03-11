baa3fb5ea8fbddd65118607263861b1a104f177b
Denis Maltev <A01171996@ad.bcit.ca>
2020-03-11 15:53:50 -0700

    get data from server in console

--- stocks/screens/Home.js
+++ stocks/screens/Home.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import finnhub from '../api/finnhub';
 
-export default function Home() {
+const Home = () => {
+  //const baseURL = 'https://finnhub.io/api/v1';
+  const [stocks, setStocks] = useState([]);
+
+  const searchAPI = async () => {
+    const response = await finnhub.get('/stock/symbol?exchange=US&token=bpkirnfrh5rcgrlrakt0', {});
+    setStocks(response.data);
+    console.log("+++++++++++++++++++");
+    console.log(response.data);
+  }
+
+  useEffect(()=>{searchAPI()},[]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -17,3 +30,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default Home;
