629e1431e3b7aa67570a40fe239bd786b6dbe60f
crystal mclean <cmclean48@my.bcit.ca>
2020-03-12 10:20:19 -0700

    attempting firebase

--- stocks/screens/Register.js
+++ stocks/screens/Register.js
@@ -1,4 +1,38 @@
-const Register = ({ route }) => {
-	return null;
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'expo-firebase-app';
+import { auth } from 'expo-firebase';
+
+const Register = ({}) => {
+	// Set an initializing state whilst Firebase connects
+	const [initializing, setInitializing] = useState(true);
+	const [user, setUser] = useState();
+
+	// Handle user state changes
+	function onAuthStateChanged(user) {
+		setUser(user);
+		if (initializing) setInitializing(false);
+	}
+
+	useEffect(() => {
+		const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+		return subscriber; // unsubscribe on unmount
+	}, []);
+
+	if (initializing) return null;
+
+	if (!user) {
+		return (
+			<View>
+				<Text>Login</Text>
+			</View>
+		);
+	}
+
+	return (
+		<View>
+			<Text>Welcome {user.email}</Text>
+		</View>
+	);
 };
 export default Register;
