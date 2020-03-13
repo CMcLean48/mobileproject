520a72dda6b4925455a18803a558871ed907bc00
Den79 <test08081979@gmail.com>
2020-03-12 18:19:35 -0700

    Home page: List of stocks from server

--- stocks/screens/Home.js
+++ stocks/screens/Home.js
@@ -1,24 +1,46 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, VirtualizedList } from 'react-native';
 import finnhub from '../api/finnhub';
 
 const Home = () => {
-  //const baseURL = 'https://finnhub.io/api/v1';
-  const [stocks, setStocks] = useState([]);
+  
+  const TOKEN = ""; //Add HERE your API-Key
 
+  const [stocks, setStocks] = useState([]);
   const searchAPI = async () => {
-    const response = await finnhub.get('/stock/symbol?exchange=US&token=bpkirnfrh5rcgrlrakt0', {});
+    const response = await finnhub.get('/stock/symbol?exchange=US&token=' + TOKEN, {});
     setStocks(response.data);
-    console.log("+++++++++++++++++++");
-    console.log(response.data);
   }
 
-  useEffect(()=>{searchAPI()},[]);
+  useEffect(() => { searchAPI() }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView  style={styles.container}>
+    <VirtualizedList
+      windowSize={10}
+      data={stocks}
+      initialNumberToRender={20}
+        renderItem={({ item }) => (
+          <Text style={styles.cell}>[{item.symbol}] {item.description}</Text>
+      )}
+      keyExtractor={(item) => {
+        return item.key;
+      }}
+      getItem={(stocks, index) => {
+        const item = stocks[index]
+        return {
+          ...item,
+          key: `Item${index}${item.id}`,
+        };
+      }}
+      сolumnWrapperStyle={{ marginBottom: 26 }}
+      numColumns={1}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      horizontal={false}
+      getItemCount={(stocks) => stocks.length}
+    />
+    </SafeAreaView >
   );
 }
 
@@ -29,6 +51,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cell: {
+    margin: 10,
+    fontSize: 16,
+    fontWeight: "bold"
+  }
 });
 
 export default Home;
