032a9f741db67db99aa2abc3870a724c18c31b73
TonyKoo <TonyKoo@msn.com>
2020-03-21 17:06:14 -0700

    Add Navigation Code

--- stocks/components/ShowList.js
+++ stocks/components/ShowList.js
@@ -1,50 +1,56 @@
-import React from 'react';
-import { Text, VirtualizedList, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { Fragment, navigation } from "react";
+import { Text, VirtualizedList, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const ShowList = ({ stocks }) => {
-	const showDetail = ({ navigation, symbol }) => {
-		// console.log(symbol);
-		navigation.navigate('Detail', symbol);
-	};
-	return (
-		<VirtualizedList
-			windowSize={10}
-			data={stocks}
-			initialNumberToRender={20}
-			renderItem={({ item }) => (
-				<TouchableOpacity onPress={() => showDetail(item.symbol)}>
-					<Text style={styles.cell}>
-						[{item.symbol}] {item.description}
-					</Text>
-				</TouchableOpacity>
-			)}
-			keyExtractor={item => {
-				return item.key;
-			}}
-			getItem={(stocks, index) => {
-				const item = stocks[index];
-				return {
-					...item,
-					key: `Item${index}${item.id}`
-				};
-			}}
-			сolumnWrapperStyle={{ marginBottom: 26 }}
-			numColumns={1}
-			showsVerticalScrollIndicator={false}
-			showsHorizontalScrollIndicator={false}
-			horizontal={false}
-			getItemCount={stocks => stocks.length}
-		/>
-	);
-};
+export default function ShowList({ navigation, stocks }) {
+  // const showDetail = ({ navigation, symbol }) => {
+  // 	// console.log(symbol);
+  // 	navigation.navigate('Detail', symbol);
+  // };
+
+  function showDetail(symbol) {
+    alert(symbol);
+
+    //Navigate to Stock Detail w/ Params
+    navigation.navigate("Stock Detail", { stock: symbol });
+  }
+
+  return (
+    <VirtualizedList
+      windowSize={10}
+      data={stocks}
+      initialNumberToRender={20}
+      renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => showDetail(item.symbol)}>
+          <Text style={styles.cell}>
+            [{item.symbol}] {item.description}
+          </Text>
+        </TouchableOpacity>
+      )}
+      keyExtractor={item => {
+        return item.key;
+      }}
+      getItem={(stocks, index) => {
+        const item = stocks[index];
+        return {
+          ...item,
+          key: `Item${index}${item.id}`
+        };
+      }}
+      сolumnWrapperStyle={{ marginBottom: 26 }}
+      numColumns={1}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      horizontal={false}
+      getItemCount={stocks => stocks.length}
+    />
+  );
+}
 
 const styles = StyleSheet.create({
-	cell: {
-		margin: 10,
-		fontSize: 16,
-		fontWeight: 'bold'
-	}
+  cell: {
+    margin: 10,
+    fontSize: 16,
+    fontWeight: "bold"
+  }
 });
-
-export default ShowList;
--- stocks/screens/Detail.js
+++ stocks/screens/Detail.js
@@ -1,36 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import finnhub from '../api/finnhub';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import finnhub from "../api/finnhub";
 
-export default function Detail({ route , navigation}) {
-	const [quote, setQuote] = useState(null);
+const API_KEY = ""; //API Key - This should probably be moved to a central file later
 
-	const searchAPI = async () => {
-		const response = await finnhub.get('/quote?symbol='(`${route.params}`));
-		{
-		}
-		setQuote(response.data);
-		console.log(response);
-	};
-	useEffect(() => {
-		searchAPI();
-	}, []);
-	if (!quote) {
-		return null;
-	}
+export default function Detail({ route, navigation }) {
+  const [quote, setQuote] = useState(null);
 
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-		</View>
-	);
+  const searchAPI = async () => {
+    const response = await finnhub
+      .get(`/quote?token=${API_KEY}&symbol=${route.params.stock}`)
+      .then(response => console.log(response.data));
+    // setQuote(response.data);
+    // console.log(response);
+  };
+  useEffect(() => {
+    searchAPI();
+
+    //Get Params from Route
+    console.log(route.params.stock);
+  }, []);
+  if (!quote) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
