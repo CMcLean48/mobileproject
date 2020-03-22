09cd509afcdaa7abc85642b45616a3c470be66ba
crystal mclean <cmclean48@my.bcit.ca>
2020-03-21 23:00:10 -0700

    render stock quote, starting candle

--- stocks/screens/Home.js
+++ stocks/screens/Home.js
@@ -1,66 +1,73 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, SafeAreaView, Button } from "react-native";
-import finnhub from "../api/finnhub";
-import SearchBar from "../components/SearchBar";
-import ShowList from "../components/ShowList";
-import Logout from "../components/Logout";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, Button } from 'react-native';
+import finnhub from '../api/finnhub';
+import SearchBar from '../components/SearchBar';
+import ShowList from '../components/ShowList';
+import Logout from '../components/Logout';
 
-import firebase from "../firebase";
+import firebase from '../firebase';
 
 export default function Home({ navigation }) {
-  const API_KEY = ""; //Add HERE your API-Key
+	const API_KEY = 'bprd3evrh5r8s3uv7k0g'; //Add HERE your API-Key
 
-  const [stocks, setStocks] = useState([]);
-  const [query, setQuery] = useState("");
+	const [stocks, setStocks] = useState([]);
+	const [query, setQuery] = useState('');
 
-  const searchAPI = async () => {
-    console.log("CALLING API");
-    const response = await finnhub.get(
-      "/stock/symbol?exchange=US&token=" + API_KEY,
-      {}
-    );
-    setStocks(response.data);
-  };
+	const searchAPI = async () => {
+		console.log('CALLING API');
+		const response = await finnhub.get(
+			'/stock/symbol?exchange=US&token=' + API_KEY,
+			{}
+		);
+		setStocks(response.data);
+	};
 
-  useEffect(() => {
-    searchAPI();
-  }, []);
+	useEffect(() => {
+		searchAPI();
+	}, []);
 
-  return (
-    <SafeAreaView style={styles.container}>
-      {/* <Text>TEST</Text> */}
-      <SearchBar
-        query={query}
-        onQueryChange={newQuery => {
-          setQuery(newQuery);
-        }}
-      />
-      <ShowList
-        navigation={navigation}
-        stocks={stocks.filter(
-          stock =>
-            stock.symbol.toUpperCase().includes(query.toUpperCase()) ||
-            stock.description.toUpperCase().includes(query.toUpperCase())
-        )}
-      />
-      <Button
-        title="Register"
-        onPress={() => navigation.navigate("Register")}
-      />
-      <Button title="Login" onPress={() => navigation.navigate("Login")} />
-      <Button
-        title="Portfolio"
-        onPress={() => navigation.navigate("Portfolio")}
-      />
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			{/* <Text>TEST</Text> */}
+			<SearchBar
+				query={query}
+				onQueryChange={newQuery => {
+					setQuery(newQuery);
+				}}
+			/>
+			<ShowList
+				navigation={navigation}
+				stocks={stocks.filter(
+					stock =>
+						stock.symbol
+							.toUpperCase()
+							.includes(query.toUpperCase()) ||
+						stock.description
+							.toUpperCase()
+							.includes(query.toUpperCase())
+				)}
+			/>
+			<Button
+				title="Register"
+				onPress={() => navigation.navigate('Register')}
+			/>
+			<Button
+				title="Login"
+				onPress={() => navigation.navigate('Login')}
+			/>
+			<Button
+				title="Portfolio"
+				onPress={() => navigation.navigate('Portfolio')}
+			/>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
