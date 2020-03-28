c25f6e5dd522fba3a74d24e4019c6689599980b8
Den79 <test08081979@gmail.com>
2020-03-28 00:14:31 -0700

    Add item to wachlist
    add to watchlist works, needs notification and status code checking

--- stocks/screens/Detail.js
+++ stocks/screens/Detail.js
@@ -1,148 +1,186 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-	StyleSheet,
-	SafeAreaView,
-	Text,
-	View,
-	Button,
-	Dimensions
-} from 'react-native';
-import finnhub from '../api/finnhub';
-import { LineChart } from 'react-native-chart-kit';
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Button,
+  Dimensions,
+  AsyncStorage
+} from "react-native";
+import finnhub from "../api/finnhub";
+import { LineChart } from "react-native-chart-kit";
 //import { FINNHUB_API_KEY } from 'react-native-dotenv';
 
-const API_KEY = 'bprd3evrh5r8s3uv7k0g'; //API Key - This should probably be moved to a central file later
+const API_KEY = "bprd3evrh5r8s3uv7k0g"; //API Key - This should probably be moved to a central file later
 
 export default function Detail({ route, navigation }) {
-	const [quote, setQuote] = useState(null);
-	const [candle, setCandle] = useState(null);
+  const [quote, setQuote] = useState(null);
+  const [candle, setCandle] = useState(null);
 
-	const searchAPI = async () => {
-		const response = await finnhub.get(
-			`/quote?token=${API_KEY}&symbol=${route.params.stock}`
-		);
-		//.then(response => (response.data));
-		setQuote(response.data);
-		console.log(response.data);
-	};
+  const searchAPI = async () => {
+    const response = await finnhub.get(
+      `/quote?token=${API_KEY}&symbol=${route.params.stock}`
+    );
+    //.then(response => (response.data));
+    setQuote(response.data);
+    console.log(response.data);
+  };
 
-	var currentDate = Math.round(new Date().getTime() / 1000);
-	let fromDate = currentDate - 2592000;
-	//console.log('current date' + currentDate);
-	//console.log('from date' + fromDate);
-	const searchAPICandle = async () => {
-		const candleResponse = await finnhub.get(
-			`/stock/candle?symbol=${route.params.stock}&resolution=D&to=${currentDate}&from=${fromDate}&token=${API_KEY}`
-		);
-		//console.log(candleResponse.data);
-		setCandle(candleResponse.data);
-	};
+  var currentDate = Math.round(new Date().getTime() / 1000);
+  let fromDate = currentDate - 2592000;
+  //console.log('current date' + currentDate);
+  //console.log('from date' + fromDate);
+  const searchAPICandle = async () => {
+    const candleResponse = await finnhub.get(
+      `/stock/candle?symbol=${route.params.stock}&resolution=D&to=${currentDate}&from=${fromDate}&token=${API_KEY}`
+    );
+    //console.log(candleResponse.data);
+    setCandle(candleResponse.data);
+  };
 
-	useEffect(() => {
-		searchAPI();
-		searchAPICandle();
-		//Get Params from Route
-		//console.log(route.params.stock);
-	}, []);
-	if (!quote) {
-		return null;
-	}
-	if (!candle) {
-		return null;
-	} else {
-		console.log('candle ' + candle.c);
-	}
-	const lineData = {
-		labels: ['week 2', 'week 1', 'current week'],
-		datasets: [
-			{
-				data: candle.c === undefined ? [0] : candle.c,
-				strokeWidth: 4 // optional
-			}
-		],
-		legend: ['closing price over 3 weeks  ']
-	};
-	const chartConfig = {
-		backgroundColor: '#e26a00',
-		backgroundGradientFrom: '#33A5FF',
-		backgroundGradientTo: '#ffa726',
-		decimalPlaces: 2, // optional, defaults to 2dp
-		color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-		style: {
-			borderRadius: 16
-		}
-	};
+  useEffect(() => {
+    searchAPI();
+    searchAPICandle();
+    //Get Params from Route
+    //console.log(route.params.stock);
+  }, []);
+  if (!quote) {
+    return null;
+  }
+  if (!candle) {
+    return null;
+  } else {
+    console.log("candle " + candle.c);
+  }
+  const lineData = {
+    labels: ["week 2", "week 1", "current week"],
+    datasets: [
+      {
+        data: candle.c === undefined ? [0] : candle.c,
+        strokeWidth: 4 // optional
+      }
+    ],
+    legend: ["closing price over 3 weeks  "]
+  };
+  const chartConfig = {
+    backgroundColor: "#e26a00",
+    backgroundGradientFrom: "#33A5FF",
+    backgroundGradientTo: "#ffa726",
+    decimalPlaces: 2, // optional, defaults to 2dp
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+      borderRadius: 16
+    }
+  };
 
-	return (
-		<>
-			<View>
-				<Text>{route.params.stock}</Text>
-				<LineChart
-					data={lineData}
-					width={Dimensions.get('window').width} // from react-native
-					height={240}
-					yAxisLabel={'$'}
-					chartConfig={chartConfig}
-					bezier
-					style={{
-						marginVertical: 8,
-						borderRadius: 16
-					}}
-				/>
-			</View>
-			<SafeAreaView style={styles.container}>
-				<Text style={styles.symbol}>Stock: {route.params.stock}</Text>
-				<View style={styles.quote}>
-					<Text style={styles.qt}>open:${quote.o}</Text>
-					<Text style={styles.qt}>close:${quote.c}</Text>
-					<Text style={styles.qt}>high:${quote.h}</Text>
-					<Text style={styles.qt}>low:${quote.l}</Text>
-					<Text style={styles.qt}>previous close:${quote.pc}</Text>
-				</View>
-				<Button title="Watch Stock" onPress={() => watchStock()} />
-				<Button
-					title="Watched Stocks"
-					onPress={() => navigation.navigate('WatchList')}
-				/>
-				<Button
-					title="Portfolio"
-					onPress={() => navigation.navigate('Portfolio')}
-				/>
+  async function getJWT() {
+    try {
+      let value = await AsyncStorage.getItem("JWT_TOKEN");
+      if (value !== null) return value;
+    } catch (error) {
+      //console.error(error);
+    }
+  }
 
-				<Button title="Buy" onPress={() => buyStock()} />
+  async function watchStock(stockSymbol) {
+    var JWTtoken = await getJWT();
+    //console.log(JWTtoken);
+    //console.log(stockSymbol);
+    if (JWTtoken !== null) {
+      await fetch("https://ssdstockappapi.azurewebsites.net/api/WatchList", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${JWTtoken}`
+        },
+        body: JSON.stringify({ stockSymbol: stockSymbol })
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      //console.log(response);
+    }
+  }
 
-				<Button title="Sell" onPress={() => sellStock()} />
-			</SafeAreaView>
-		</>
-	);
+  return (
+    <>
+      <View>
+        <Text>{route.params.stock}</Text>
+        <LineChart
+          data={lineData}
+          width={Dimensions.get("window").width} // from react-native
+          height={240}
+          yAxisLabel={"$"}
+          chartConfig={chartConfig}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+      </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.symbol}>Stock: {route.params.stock}</Text>
+        <View style={styles.quote}>
+          <Text style={styles.qt}>open:${quote.o}</Text>
+          <Text style={styles.qt}>close:${quote.c}</Text>
+          <Text style={styles.qt}>high:${quote.h}</Text>
+          <Text style={styles.qt}>low:${quote.l}</Text>
+          <Text style={styles.qt}>previous close:${quote.pc}</Text>
+        </View>
+        <Button
+          title="Watch Stock"
+          onPress={() => watchStock(route.params.stock)}
+        />
+        <Button
+          title="Watched Stocks"
+          onPress={() => navigation.navigate("WatchList")}
+        />
+        <Button
+          title="Portfolio"
+          onPress={() => navigation.navigate("Portfolio")}
+        />
+
+        <Button title="Buy" onPress={() => buyStock()} />
+
+        <Button title="Sell" onPress={() => sellStock()} />
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
+  container: {
+    flex: 1,
 
-		backgroundColor: '#ff8a3c',
-		alignItems: 'center'
-		//	justifyContent: 'center'
-	},
-	symbol: {
-		paddingTop: 20,
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-		color: '#fff',
-		fontSize: 25
-	},
-	quote: {
-		flexDirection: 'row',
-		flexWrap: 'wrap',
-		justifyContent: 'center',
-		paddingTop: 40
-	},
-	qt: {
-		color: '#fff',
-		fontSize: 20,
-		justifyContent: 'space-between',
-		paddingRight: 20,
-		paddingTop: 20
-	}
+    backgroundColor: "#ff8a3c",
+    alignItems: "center"
+    //	justifyContent: 'center'
+  },
+  symbol: {
+    paddingTop: 20,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    color: "#fff",
+    fontSize: 25
+  },
+  quote: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    paddingTop: 40
+  },
+  qt: {
+    color: "#fff",
+    fontSize: 20,
+    justifyContent: "space-between",
+    paddingRight: 20,
+    paddingTop: 20
+  }
 });
--- stocks/screens/Portfolio.js
+++ stocks/screens/Portfolio.js
@@ -1,93 +1,98 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, SafeAreaView, Button } from 'react-native';
-import firebase from '../firebase';
+import React, { useState } from "react";
+import { StyleSheet, Text, SafeAreaView, Button } from "react-native";
+import firebase from "../firebase";
 import { useFocusEffect } from "@react-navigation/native";
 import { AsyncStorage } from "react-native";
 
-
-
 export default function Portfolio({ navigation }) {
+  useFocusEffect(
+    React.useCallback(() => {
+      let isActive = true;
 
-	useFocusEffect(
-		React.useCallback(() => {
-		  let isActive = true;
+      async function getJWT() {
+        try {
+          console.log("inside retrive data");
+          let value = await AsyncStorage.getItem("JWT_TOKEN");
+          if (value !== null) {
+            // We have data!!
+            console.log(value);
+            return value;
+          }
+        } catch (error) {
+          // Error retrieving data
+          console.error(error);
+        }
+      }
+      async function getData(isActive) {
+        if (isActive) {
+          fetch("https://ssdstockappapi.azurewebsites.net/api/Portfolio", {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${await getJWT()}`
+            }
+          })
+            .then(res => res.json())
+            .then(data => {
+              setjson(data);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }
+      }
 
-		  async function getJWT() {
-			try {
-			  console.log("inside retrive data");
-			  let value = await AsyncStorage.getItem("JWT_TOKEN");
-			  if (value !== null) {
-				// We have data!!
-				console.log(value);
-				return value;
-			  }
-			} catch (error) {
-			  // Error retrieving data
-			  console.error(error);
-			}
-		  }
-		  async function getData(isActive) {
-			if (isActive) {
-			  fetch("https://ssdstockappapi.azurewebsites.net/api/Portfolio", {
-				method: "GET",
-				headers: {
-				  Authorization: `Bearer ${await getJWT()}`
-				}
-			  })
-				.then(res => res.json())
-				.then(data => {
-				  setjson(data);
-				})
-				.catch(error => {
-				  console.log(error);
-				});
-			}
-		  }
+      getData(isActive).catch(e => {
+        console.error(error);
+      });
 
+      return () => {
+        isActive = false;
+      };
+    }, [])
+  );
 
+  const [json, setjson] = useState({
+    userEmail: "Loading"
+  });
 
-		  getData(isActive).catch(e => {console.error(error)})
-
-		  return () => {
-			isActive = false;
-		  };
-		}, [])
-	  );
-
-	const [json, setjson] = useState({
-		userEmail: "Loading"
-	  })
-
-	return(
-		<SafeAreaView style={styles.container}>
-			<Text>{json.userEmail}</Text>
-			{!!json.cashBalance && <Text>Cash Balance: $ {json.cashBalance} USD</Text>}
-			{!!json.currentPortfolioValue && <Text>Portfolio Value: $ {json.currentPortfolioValue} CAD</Text>}
-			{!!json.stockHoldings && json.stockHoldings.map((stock) => {
-				return ( <SafeAreaView key={stock.stockSymbol} style={styles.stocks}>
-				<Text style={styles.bold}>{stock.companyName} | {stock.stockSymbol}</Text>
-						<Text>Total Value: {stock.currentValue}</Text>
-						<Text>Stock Price: {stock.currentPrice}</Text>
-						<Text>Quantity: {stock.quantity}</Text>
-
-						</SafeAreaView>
-					)
-			})}
-		</SafeAreaView>);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>{json.userEmail}</Text>
+      {!!json.cashBalance && (
+        <Text>Cash Balance: $ {json.cashBalance} USD</Text>
+      )}
+      {!!json.currentPortfolioValue && (
+        <Text>Portfolio Value: $ {json.currentPortfolioValue} CAD</Text>
+      )}
+      {!!json.stockHoldings &&
+        json.stockHoldings.map(stock => {
+          return (
+            <SafeAreaView key={stock.stockSymbol} style={styles.stocks}>
+              <Text style={styles.bold}>
+                {stock.companyName} | {stock.stockSymbol}
+              </Text>
+              <Text>Total Value: {stock.currentValue}</Text>
+              <Text>Stock Price: {stock.currentPrice}</Text>
+              <Text>Quantity: {stock.quantity}</Text>
+            </SafeAreaView>
+          );
+        })}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-	  flex: 1,
-	  backgroundColor: "#fff",
-	  alignItems: "center",
-	  justifyContent: "center"
-	},
-	stocks: {
-		alignItems: "center"
-	},
-	bold: {
-		paddingTop: 10,
-		fontWeight: "bold"
-	}
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  stocks: {
+    alignItems: "center"
+  },
+  bold: {
+    paddingTop: 10,
+    fontWeight: "bold"
+  }
+});
