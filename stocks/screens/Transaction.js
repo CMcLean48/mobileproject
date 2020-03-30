cdf06039a1276bfa0163d4f00c0e442907c3c9c5
Lucas Johnson <woodsman.lucas@gmail.com>
2020-03-30 14:07:21 -0700

    Ready for business

--- stocks/screens/Transaction.js
+++ stocks/screens/Transaction.js
@@ -1,14 +1,71 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import firebase from "../firebase";
+
 
 export default function Transaction({navigation, route}) {
+
+		  async function getJWT() {
+			try {
+			  console.log("inside retrive data");
+			  let value = await firebase.auth().currentUser.getIdTokenResult()
+			  if (value.token !== null) {
+				// We have data!!
+				console.log(value);
+				return value.token;
+			  }
+			} catch (error) {
+			  // Error retrieving data
+			  console.error(error);
+			}
+		  }
+
+
+      async function getBuy() {
+          fetch("https://ssdstockappapi.azurewebsites.net/api/StockTransaction/purchase", {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${await getJWT()}`
+            },
+            body: JSON.stringify({
+              StockSymbol: route.params.stock,
+              quantity: parseInt(value)
+            })
+          })
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+      }
+
+      async function getSell(isActive) {
+        if (isActive) {
+          fetch("https://ssdstockappapi.azurewebsites.net/api/StockTransaction/sell", {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${await getJWT()}`
+            }
+          })
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }
+      }
 
   const [value, onChangeText] = useState("");
 
   return (
     <View style={styles.container}>
       <Text>{route.params.stock}</Text>
-      <Text>Buy Stock Qty</Text>
+      <Text>Stock Qty</Text>
 				<TextInput
 
 					keyboardType={'numeric'}
@@ -16,6 +73,8 @@ export default function Transaction({navigation, route}) {
 					onChangeText={text => onChangeText(text)}
 					value={value}
 				/>
+        <Button title="Buy" onPress={getBuy} />
+        <Button title="Sell" onPress={getSell} />
     </View>
   );
 }
