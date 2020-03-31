97574531cb8b0e6acdbc0553fa42fe37af659073
crystal mclean <cmclean48@my.bcit.ca>
2020-03-30 19:38:15 -0700

    added watchlist button

--- stocks/screens/Portfolio.js
+++ stocks/screens/Portfolio.js
@@ -1,214 +1,224 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  TouchableOpacity,
-  Alert,
-  Dimensions,
-  ScrollView
-} from "react-native";
-import { useFocusEffect } from "@react-navigation/native";
-import { LineChart } from "react-native-chart-kit";
-import firebase from "../firebase";
+	StyleSheet,
+	Text,
+	SafeAreaView,
+	TouchableOpacity,
+	Alert,
+	Dimensions,
+	ScrollView,
+	Button
+} from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { LineChart } from 'react-native-chart-kit';
+import firebase from '../firebase';
 
 export default function Portfolio({ navigation }) {
-  useFocusEffect(
-    React.useCallback(() => {
-      let isActive = true;
+	useFocusEffect(
+		React.useCallback(() => {
+			let isActive = true;
 
-      // async function getJWT() {
-      // try {
-      //   console.log("inside retrive data");
-      //   let value = await firebase.auth().currentUser.getIdTokenResult()
-      //   if (value.token !== null) {
-      // 	// We have data!!
-      // 	console.log(value);
-      // 	return value.token;
-      //   }
-      // } catch (error) {
-      //   // Error retrieving data
-      //   console.error(error);
-      // }
-      // }
+			// async function getJWT() {
+			// try {
+			//   console.log("inside retrive data");
+			//   let value = await firebase.auth().currentUser.getIdTokenResult()
+			//   if (value.token !== null) {
+			// 	// We have data!!
+			// 	console.log(value);
+			// 	return value.token;
+			//   }
+			// } catch (error) {
+			//   // Error retrieving data
+			//   console.error(error);
+			// }
+			// }
 
-      async function getData(isActive) {
-        let firebaseToken = await firebase
-          .auth()
-          .currentUser.getIdTokenResult();
+			async function getData(isActive) {
+				let firebaseToken = await firebase
+					.auth()
+					.currentUser.getIdTokenResult();
 
-        if (isActive) {
-          fetch("https://ssdstockappapi.azurewebsites.net/api/Portfolio", {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${firebaseToken.token}`
-            }
-          })
-            .then(res => res.json())
-            .then(data => {
-              setjson(data);
-              console.log(data);
-            })
-            .catch(error => {
-              console.log(error);
-            });
-        }
-      }
+				if (isActive) {
+					fetch(
+						'https://ssdstockappapi.azurewebsites.net/api/Portfolio',
+						{
+							method: 'GET',
+							headers: {
+								Authorization: `Bearer ${firebaseToken.token}`
+							}
+						}
+					)
+						.then(res => res.json())
+						.then(data => {
+							setjson(data);
+							console.log(data);
+						})
+						.catch(error => {
+							console.log(error);
+						});
+				}
+			}
 
-      getData(isActive).catch(e => {
-        console.error(error);
-      });
+			getData(isActive).catch(e => {
+				console.error(error);
+			});
 
-      return () => {
-        isActive = false;
-      };
-    }, [])
-  );
+			return () => {
+				isActive = false;
+			};
+		}, [])
+	);
 
-  const [json, setjson] = useState({
-    userEmail: "Loading"
-  });
+	const [json, setjson] = useState({
+		userEmail: 'Loading'
+	});
 
-  function displayMore(stock) {
-    Alert.alert(
-      `${stock.companyName}`,
-      `Avg Cost: $ ${stock.averageCost.toFixed(2)}
+	function displayMore(stock) {
+		Alert.alert(
+			`${stock.companyName}`,
+			`Avg Cost: $ ${stock.averageCost.toFixed(2)}
 Total Cost: $ ${stock.totalCost.toFixed(2)}
 Current Price: $ ${stock.currentPrice.toFixed(2)}
 Current Value: $ ${stock.currentValue.toFixed(2)}
 Unrealized Gain/Loss: $ ${stock.unrealizedGainLoss.toFixed(2)}
 `
-    );
-  }
+		);
+	}
 
-  const lineData = {
-    labels: [],
-    datasets: [
-      {
-        data: json.portfolio30DayHistory
-          ? json.portfolio30DayHistory.map(history => {
-              if (history.basePortfolioValue) {
-                return (
-                  ((history.snapshotPortfolioValue -
-                    history.basePortfolioValue) /
-                    history.basePortfolioValue) *
-                  100
-                );
-              } else {
-                return 0;
-              }
-            })
-          : [0],
-        strokeWidth: 4 // optional
-      }
-    ],
-    legend: ["percentage gain / loss over 4 weeks  "]
-  };
+	const lineData = {
+		labels: [],
+		datasets: [
+			{
+				data: json.portfolio30DayHistory
+					? json.portfolio30DayHistory.map(history => {
+							if (history.basePortfolioValue) {
+								return (
+									((history.snapshotPortfolioValue -
+										history.basePortfolioValue) /
+										history.basePortfolioValue) *
+									100
+								);
+							} else {
+								return 0;
+							}
+					  })
+					: [0],
+				strokeWidth: 4 // optional
+			}
+		],
+		legend: ['percentage gain / loss over 4 weeks  ']
+	};
 
-  const chartConfig = {
-    backgroundColor: "#e26a00",
-    backgroundGradientFrom: "#33A5FF",
-    backgroundGradientTo: "#ffa726",
-    decimalPlaces: 2, // optional, defaults to 2dp
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    style: {
-      borderRadius: 16
-    }
-  };
+	const chartConfig = {
+		backgroundColor: '#e26a00',
+		backgroundGradientFrom: '#33A5FF',
+		backgroundGradientTo: '#ffa726',
+		decimalPlaces: 2, // optional, defaults to 2dp
+		color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+		style: {
+			borderRadius: 16
+		}
+	};
 
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <LineChart
-        data={lineData}
-        width={Dimensions.get("window").width} // from react-native
-        height={240}
-        yAxisLabel={"% "}
-        chartConfig={chartConfig}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16
-        }}
-      />
-      <ScrollView contentContainerStyle={styles.scroll}>
-        <SafeAreaView style={styles.textContainer}>
-          <Text style={styles.textBold}>{json.userEmail}</Text>
-          {!!json.cashBalance && (
-            <Text style={styles.text}>
-              Cash Balance: $ {json.cashBalance} USD
-            </Text>
-          )}
-          {!!json.currentPortfolioValue && (
-            <Text style={styles.text}>
-              Portfolio Value: $ {json.currentPortfolioValue} USD
-            </Text>
-          )}
-        </SafeAreaView>
-        {!!json.stockHoldings &&
-          json.stockHoldings.map((stock, index) => {
-            return (
-              <TouchableOpacity
-                onPress={() => {
-                  displayMore(stock);
-                }}
-                key={stock.stockSymbol}
-                style={{
-                  backgroundColor: checkIndexIsEven(index)
-                    ? "#ff8a3c"
-                    : "#33A5FF",
-                  paddingBottom: 10,
-                  borderRadius: 10,
-                  width: 250,
-                  alignItems: "center",
-                  marginTop: 10
-                }}
-              >
-                <Text style={styles.bold}>
-                  {stock.companyName} | {stock.stockSymbol}
-                </Text>
-                <Text>Stock Price: $ {stock.currentPrice.toFixed(2)}</Text>
-                <Text>Quantity: {stock.quantity}</Text>
-                <Text>Click for more info</Text>
-              </TouchableOpacity>
-            );
-          })}
-      </ScrollView>
-    </SafeAreaView>
-  );
-
+	return (
+		<SafeAreaView style={styles.container}>
+			<LineChart
+				data={lineData}
+				width={Dimensions.get('window').width} // from react-native
+				height={240}
+				yAxisLabel={'% '}
+				chartConfig={chartConfig}
+				bezier
+				style={{
+					marginVertical: 8,
+					borderRadius: 16
+				}}
+			/>
+			<ScrollView contentContainerStyle={styles.scroll}>
+				<SafeAreaView style={styles.textContainer}>
+					<Text style={styles.textBold}>{json.userEmail}</Text>
+					{!!json.cashBalance && (
+						<Text style={styles.text}>
+							Cash Balance: $ {json.cashBalance} USD
+						</Text>
+					)}
+					{!!json.currentPortfolioValue && (
+						<Text style={styles.text}>
+							Portfolio Value: $ {json.currentPortfolioValue} USD
+						</Text>
+					)}
+					<Button
+						color="#33A5FF"
+						title="WatchList"
+						onPress={() => navigation.navigate('WatchList')}
+					/>
+				</SafeAreaView>
+				{!!json.stockHoldings &&
+					json.stockHoldings.map((stock, index) => {
+						return (
+							<TouchableOpacity
+								onPress={() => {
+									displayMore(stock);
+								}}
+								key={stock.stockSymbol}
+								style={{
+									backgroundColor: checkIndexIsEven(index)
+										? '#ff8a3c'
+										: '#33A5FF',
+									paddingBottom: 10,
+									borderRadius: 10,
+									width: 250,
+									alignItems: 'center',
+									marginTop: 10
+								}}
+							>
+								<Text style={styles.bold}>
+									{stock.companyName} | {stock.stockSymbol}
+								</Text>
+								<Text>
+									Stock Price: ${' '}
+									{stock.currentPrice.toFixed(2)}
+								</Text>
+								<Text>Quantity: {stock.quantity}</Text>
+								<Text>Click for more info</Text>
+							</TouchableOpacity>
+						);
+					})}
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
 
 function checkIndexIsEven(n) {
-  return n % 2 == 0;
+	return n % 2 == 0;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "#fff",
-    justifyContent: "center"
-  },
-  textContainer: {
-    alignItems: "center"
-  },
-  text: {
-    width: "80%",
-    alignItems: "center"
-  },
-  scroll: {
-    marginHorizontal: 0,
-    width: "100%",
-    alignItems: "center"
-  },
-  textBold: {
-    width: "80%",
-    alignItems: "center",
-    fontWeight: "bold"
-  },
-  bold: {
-    paddingTop: 10,
-    fontWeight: "bold"
-  }
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		backgroundColor: '#fff',
+		justifyContent: 'center'
+	},
+	textContainer: {
+		alignItems: 'center'
+	},
+	text: {
+		width: '80%',
+		alignItems: 'center'
+	},
+	scroll: {
+		marginHorizontal: 0,
+		width: '100%',
+		alignItems: 'center'
+	},
+	textBold: {
+		width: '80%',
+		alignItems: 'center',
+		fontWeight: 'bold'
+	},
+	bold: {
+		paddingTop: 10,
+		fontWeight: 'bold'
+	}
 });
